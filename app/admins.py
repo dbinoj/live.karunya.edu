c5249ec3218d704e0aeeaf3d411ca2baf123c9fa
Binoj David <dbinoj@gmail.com>
2016-06-09 18:20:23 +0530

    implement slug generation

--- app/admins.py
+++ app/admins.py
@@ -1,5 +1,5 @@
 from flask_admin.contrib.sqla import ModelView
-from app import db, admin
+from app import db, admin, utils
 from app.models import Event
 from flask.ext.login import current_user
 from flask import redirect, url_for, request
@@ -7,7 +7,11 @@ from flask import redirect, url_for, request
 
 class EventAdmin(ModelView):
     column_display_pk = True
-
+    form_widget_args = {
+        'slug':{
+            'disabled':True
+        }
+    }
     def is_accessible(self):
         return current_user.is_authenticated()
 
@@ -15,7 +19,8 @@ class EventAdmin(ModelView):
         # redirect to login page if user doesn't have access
         return redirect(url_for('login', next=request.url))
 
-    def on_model_change(self, form, instance):
-        pass
+    def on_model_change(self, form, instance, is_created):
+        instance.slug = utils.slugify(form.title.data)
+        super(EventAdmin, self).on_model_change(form, instance, is_created)
 
 admin.add_view(EventAdmin(Event, db.session))
